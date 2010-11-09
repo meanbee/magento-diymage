43bce7309ce196a04c37b5062723b77d9fc71e3c
Nick Jones <nick@nicksays.co.uk>
2010-11-09 07:05:48 +0000

    Control the loading of the builder from the diy.xml, not the layout.xml

--- app/code/local/Meanbee/Diy/Block/Admin/Control/Builder.php
+++ app/code/local/Meanbee/Diy/Block/Admin/Control/Builder.php
@@ -1,5 +1,10 @@
 <?php
 class Meanbee_Diy_Block_Admin_Control_Builder extends Meanbee_Diy_Block_Admin_Control_Abstract {
+    public function __construct() {
+        parent::__construct();
+        $this->setTemplate('diy/controls/builder.phtml');
+    }
+    
     public function getLayoutReferenceJson($name) {
         $layout = Mage::getModel('diy/layout')->addHandle(Mage::registry('diy_current_template'));
         return Zend_Json::encode($layout->getReference($name));
