009c668b34a2306032d9a4d01f6bb43db3ab472c
Nick Jones <nick@nicksays.co.uk>
2010-11-10 20:57:33 +0000

    Helper program to set the symlinks when you clone the repository into a /diymage/ directory at the root of your Magento installation.

--- set_links.sh
+++ set_links.sh
@@ -1,19 +1,29 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+cd $PWD
 cd app/code/local/Meanbee
 ln -s ../../../../diymage/app/code/local/Meanbee/Diy/ Diy
 
+cd $PWD
 cd app/etc/modules
 ln -s ../../../diymage/app/etc/modules/Meanbee_Diy.xml .
 
+cd $PWD
 cd app/design/adminhtml/default/default/layout
 ln -s ../../../../../../diymage/app/design/adminhtml/default/default/layout/diy.xml .
 cd ../template/
 ln -s ../../../../../../diymage/app/design/adminhtml/default/default/template/diy/ diy
 
+cd $PWD
 cd app/design/frontend
 ln -s ../../../diymage/app/design/frontend/diy .
 
+cd $PWD
 cd skin/adminhtml/default/default
 ln -s ../../../../diymage/skin/adminhtml/default/default/diy/ diy
 
+cd $PWD
 cd skin/frontend
 ln -s ../../diymage/skin/frontend/diy .
