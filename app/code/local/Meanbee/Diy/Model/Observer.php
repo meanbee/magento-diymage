8103e615675fdf07ffba09c47b15a1cf0b80d6f7
Nick Jones <nick@nicksays.co.uk>
2010-11-11 07:13:19 +0000

    Attempting restructure.

--- app/code/local/Meanbee/Diy/Model/Observer.php
+++ app/code/local/Meanbee/Diy/Model/Observer.php
@@ -1,5 +1,13 @@
 <?php
 class Meanbee_Diy_Model_Observer {
+    /**
+     * The function listens out for controller_action_layout_generate_xml_before
+     *
+     * @see Mage_Core_Controller_Varien_Action
+     * @param string $observer 
+     * @return void
+     * @author Nicholas Jones
+     */
     public function layoutUpdates($observer) {
         $handle = Mage::getModel('diy/observer_layout');
         $handle->observe($observer);
--- app/code/local/Meanbee/Diy/Model/Observer/Layout.php
+++ app/code/local/Meanbee/Diy/Model/Observer/Layout.php
@@ -1,5 +1,7 @@
 <?php
 class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_Interface {
+    protected $_removedBlocks = array();
+    
     public function observe($observer) {
         $action_obj = $observer->getAction();
         $layout = $observer->getLayout();
@@ -22,10 +24,14 @@ class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_In
         
         $this->_modifyPageLayout($identifiers, $layout);
         $this->_removeBlocks($identifiers, $layout);
+        $this->_sortBlocks($identifiers, $layout);
         
         if (!Mage::helper('diy')->getValue("global", "show_categories")) {
             $this->_removeBlock($layout, "catalog.topnav");
         }
+        
+        // Apply our sort changes..
+        //$update->load();
     }
     
     /**
@@ -72,6 +78,10 @@ class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_In
                 foreach ($update as $group => $data) {
                     $remove = Zend_Json::decode($data['remove']);
                     
+                    if ($remove == null) {
+                        $remove = array();
+                    }
+                    
                     $to_remove = array_merge($to_remove, $remove);
                 }
             }
@@ -79,8 +89,150 @@ class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_In
         
         if (count($to_remove) > 0) {
             $to_remove = array_unique($to_remove);
+            
+            $this->_removedBlocks = $to_remove;
+            
             foreach ($to_remove as $block_name) {
                 $this->_removeBlock($layout, $block_name);
+            }
+        }
+    }
+    
+    /**
+     * Sort the blocks in the references.
+     *
+     * @param string $identifiers 
+     * @param string $layout 
+     * @return void
+     * @author Nicholas Jones
+     */
+    protected function _sortBlocks($identifiers, $layout) {
+        $simple_xml = $layout->getUpdate()->asSimpleXml();
+        
+        foreach ($identifiers as $identifier) {
+            $update = Zend_Json::decode(Mage::helper('diy')->getValue($identifier, "builder"));
+            
+            if (count($update) > 0) {
+                foreach ($update as $group => $data) {
+                    $blocks = Zend_Json::decode($data['sort_order']);
+                    
+                    foreach ($blocks as $key => $block_data) {
+                        
+                        // We need to establish the type of the block
+                        $element = $this->_identifyBlockType($identifiers, $layout, $block_data['name']);
+                        
+                        $type = $element->getAttribute('type');
+                        $before = $element->getAttribute('before');
+                        $template = $element->getAttribute('template');
+                        $as = $element->getAttribute('as');
+                        
+                        // An array of xml snippets
+                        $updates = $layout->getUpdate()->asArray();
+                        
+                        $block_found = array();
+                        $modified_updates = array();
+                        
+                        foreach ($updates as $update_number => &$update) {
+                            $lines = explode("\n", $update);
+                            foreach ($lines as $line_number => &$line) {
+                                $line = trim($line);
+                                if (substr($line, 0, 6) == "<block") {
+                                    $parts = explode(" ", $line);
+                                    foreach ($parts as $part) {
+                                        $subparts = explode("=", $part);
+                                        
+                                        if (count($subparts) != 2) {
+                                            continue;
+                                        }
+                                        
+                                        $key = $subparts[0];
+                                        $value = str_replace(">", "", $subparts[1]);
+                                        
+                                        if (!isset($block_found[$block_data['name']])) {
+                                            $block_found[$block_data['name']] = 0;                                            
+                                        }
+
+                                        if ($key == "name" && $value == '"' . $block_data['name'] . '"') {
+                                            $block_found[$block_data['name']] = 1;
+                                            
+                                            $after_string = 'diymage="1" after="' . $block_data['after'] . '"';
+                                            $before_string = 'before="' . $block_data['before'] . '"';
+                                            
+                                            $pattern_b = '/before=["\'].*?["\']/i';
+                                            $line = preg_replace($pattern_b, $before_string, $line, -1, $count_b);
+                                            
+                                            // If the line contains an after="", then we replace it..
+                                            $count_a = 0;
+                                            $pattern_a = '/after=["\'].*?["\']/i';
+                                            $updated_line = preg_replace($pattern_a, $after_string, $line, -1, $count_a);
+                                            
+                                            if ($count_a == 0) {
+                                                // .. otherwise, just add it in
+                                                if (substr($line, strlen($line) - 2) == "/>") {
+                                                    $updated_line = substr($line, 0, -2) . " " . $after_string . " />";
+                                                } else {
+                                                    $updated_line = substr($line, 0, -1) . " " . $after_string . ">";
+                                                }
+                                            }
+                                            
+                                            if ($count_b == 0) {
+                                                if (substr($updated_line, strlen($updated_line) - 2) == "/>") {
+                                                    $updated_line = substr($updated_line, 0, -2) . " " . $before_string . " />";
+                                                } else {
+                                                    $updated_line = substr($updated_line, 0, -1) . " " . $before_string . ">";
+                                                }
+                                            }
+                                            
+                                            $line = $updated_line;
+                                            
+                                            $modified_updates[] = $update_number; 
+                                        }
+                                    }                       
+                                }
+                            }
+                            
+                            $update = implode("\n", $lines);
+                        }
+                        
+                        // Put the modified updates at the end.
+                        if (count($modified_updates) > 0) {
+                            foreach ($modified_updates as $idx) {
+                                $updates[] = $updates[$idx];
+                                unset($updates[$idx]);
+                            }
+                        }
+                        
+                        echo count($updates);
+                        
+                        // print_r($block_found);
+                        // print_r($updates);
+                        
+                        // Substitution seems to be happening above, but not taking effect in the
+                        // updates when reapplied below..
+                        
+                        $layout->getUpdate()->resetUpdates();
+                        foreach ($updates as $update) {
+                            $layout->getUpdate()->addUpdate($update);
+                        }
+                        
+                        echo "<pre>";
+                        //print_r($updates);
+                        echo "</pre>";
+                        
+                        
+                        // $this->_addBlock(
+                        //     $layout,
+                        //     $group,
+                        //     $block_data['name'],
+                        //     $type, // type
+                        //     $block_data['after'],
+                        //     $before,
+                        //     $template,
+                        //     $as
+                        // );
+                        
+                    }
+                }
             }
         }
     }
@@ -129,5 +281,63 @@ class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_In
         $layout->getUpdate()->addUpdate(
             '<remove name="' . $name . '" />'
         );
+    }
+    
+    /**
+     * Utility function to generate the XML to add a block
+     *
+     * @param string $layout 
+     * @param string $reference
+     * @param string $name 
+     * @param string $type 
+     * @param string $after 
+     * @param string $before 
+     * @param string $template 
+     * @param string $as 
+     * @return void
+     * @author Nicholas Jones
+     */
+    protected function _addBlock($layout, $reference, $name, $type, $after = null, $before = null, $template = null, $as = null) {
+        $arguments = array(
+            "after", "before", "template", "as"
+        );
+        
+        $attributes = array(
+            "name" => $name,
+            "type" => $type
+        );
+        
+        foreach ($arguments as $argument) {
+            if ($$argument) {
+                $attributes[$argument] = $$argument;
+            }
+        }
+        
+        $xml = "<block ";
+        
+        foreach ($attributes as $key => $value) {
+            $xml .= $key . "='". $value."' ";
+        }
+        
+        $xml .= "/>";
+        
+        $xml = "<reference name='" . $reference . "'>" . $xml . "</reference>";
+        
+        $layout->getUpdate()->addUpdate($xml);
+    }
+    
+    protected $_compiledEarlyLayout = null;
+    protected function _identifyBlockType($identifiers, $layout, $name) {
+        if ($this->_compiledEarlyLayout == null) {
+            $this->_compiledEarlyLayout = $layout->getUpdate()->load($identifiers)->asSimpleXml();
+        }
+        
+        $xpath = "reference/block[@name='{$name}']";
+        
+        $result = $this->_compiledEarlyLayout->xpath($xpath);
+        
+        if (count($result) > 0) {
+            return $result[0];
+        }
     }
 }
