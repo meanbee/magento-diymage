455699e4d287ae3d61ec98a5f92fa245eb36a8d1
Nick Jones <nick@nicksays.co.uk>
2010-11-13 23:57:51 +0000

    [In Progress:2] Make the layout change happen last.

--- app/code/local/Meanbee/Diy/Model/Observer/Layout.php
+++ app/code/local/Meanbee/Diy/Model/Observer/Layout.php
@@ -22,10 +22,10 @@ class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_In
         
         $store_id = Mage::app()->getStore()->getStoreId();
         
-        $this->_modifyPageLayout($identifiers, $layout);
         $this->_sortBlocks($identifiers, $layout);
         $this->_removeBlocks($identifiers, $layout);
         $this->_addStaticBlocks($identifiers, $layout);
+        $this->_modifyPageLayout($identifiers, $layout);
         
         if (!Mage::helper('diy')->getValue("global", "show_categories")) {
             $this->_removeBlock($layout, "catalog.topnav");
