e54eebcaf4ff200170594c0229c76876b1900e26
Nick Jones <nick@nicksays.co.uk>
2010-11-14 11:13:22 +0000

    [Completed: 9] Added "Reset to default" for controls.

--- app/code/local/Meanbee/Diy/Block/Admin/Control/Abstract.php
+++ app/code/local/Meanbee/Diy/Block/Admin/Control/Abstract.php
@@ -1,7 +1,7 @@
 <?php
 class Meanbee_Diy_Block_Admin_Control_Abstract extends Meanbee_Diy_Block_Admin_Abstract {
     public function getFieldName() {
-        $id = $this->getControl()->getId();
+        $id = $this->getUniqueId();
         return "diy[$id]";
     }
     
@@ -23,5 +23,32 @@ class Meanbee_Diy_Block_Admin_Control_Abstract extends Meanbee_Diy_Block_Admin_A
     
     public function getValue() {
         return $this->getControl()->getValue();
+    }
+    
+    public function getUniqueId() {
+        return $this->getControl()->getId();
+    }
+    
+    /**
+     * Get the default value, as specified in the diy.xml
+     *
+     * @TODO: Use a variable store_id
+     *
+     * @return string
+     * @author Nicholas Jones
+     */
+    public function getDefaultValue() {
+        $xml = Mage::getModel("diy/config")->getAttributes();
+        
+        $name = $this->getName();
+        $group = Mage::registry('diy_current_template');
+        
+        if ($xml[$group]) {
+            if ($xml[$group][$name]) {
+                return $xml[$group][$name]["value"];
+            }
+        }
+        
+        return false;
     }
 }
---
+++ skin/adminhtml/default/default/diy/js/diymage.js
@@ -0,0 +1,4 @@
+// Used in the control section to reset the value to the origin value
+function resetValue(id, value) {
+    $j("#" + id).val(value);
+}
