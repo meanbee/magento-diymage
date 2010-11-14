47cfa6158152f0359fe95fe48ed597fdedd76970
Nick Jones <nick@nicksays.co.uk>
2010-11-14 00:53:55 +0000

    Add a method for pretty-printing XML

--- app/code/local/Meanbee/Diy/Model/Observer/Layout.php
+++ app/code/local/Meanbee/Diy/Model/Observer/Layout.php
@@ -373,4 +373,38 @@ class Meanbee_Diy_Model_Observer_Layout implements Meanbee_Diy_Model_Observer_In
             }
         }
     }
+    
+    // http://www.imarc.net/communique/148-xml_pretty_printer_in_php5
+    private function __xmlpp($xml, $html_output=false) {
+        $xml_obj = new SimpleXMLElement($xml);
+        $level = 4;
+        $indent = 0; // current indentation level
+        $pretty = array();
+
+        // get an array containing each XML element
+        $xml = explode("\n", preg_replace('/>\s*</', ">\n<", $xml_obj->asXML()));
+
+        // shift off opening XML tag if present
+        if (count($xml) && preg_match('/^<\?\s*xml/', $xml[0])) {
+          $pretty[] = array_shift($xml);
+        }
+
+        foreach ($xml as $el) {
+          if (preg_match('/^<([\w])+[^>\/]*>$/U', $el)) {
+              // opening tag, increase indent
+              $pretty[] = str_repeat(' ', $indent) . $el;
+              $indent += $level;
+          } else {
+            if (preg_match('/^<\/.+>$/', $el)) {            
+              $indent -= $level;  // closing tag, decrease indent
+            }
+            if ($indent < 0) {
+              $indent += $level;
+            }
+            $pretty[] = str_repeat(' ', $indent) . $el;
+          }
+        }   
+        $xml = implode("\n", $pretty);   
+        return ($html_output) ? htmlentities($xml) : $xml;
+    }
 }
