211c40e78221ae2ce30d9f1d031ca6ad75205eb3
Tom Robertshaw <tom.robertshaw@meanbee.com>
2010-11-17 12:00:37 +0000

    Added courier

--- app/code/local/Meanbee/Diy/Model/Source/Font.php
+++ app/code/local/Meanbee/Diy/Model/Source/Font.php
@@ -2,10 +2,11 @@
 class Meanbee_Diy_Model_Source_Font implements Meanbee_Diy_Model_Source_Interface {
     public function asArray() {
         $result = array(
-            "georgia",
             "arial",
-            "verdana",
-            "tahoma"
+            "courier",
+            "georgia",
+            "tahoma",
+            "verdana"
         );
         
         return $result;
