d7bee904dc0ebd55d96084228934b7bda194d91d
Tom Robertshaw <tom.robertshaw@meanbee.com>
2010-11-21 12:40:28 +0000

    Added advanced search page.

---
+++ app/code/local/Meanbee/Diy/Block/Admin/Design/Advancedsearch.php
@@ -0,0 +1,6 @@
+<?php
+class Meanbee_Diy_Block_Admin_Design_Advancedsearch extends Meanbee_Diy_Block_Admin_Design_Abstract {
+    protected function getDataGroup() {
+        return "catalogsearch_advanced";
+    }
+}
