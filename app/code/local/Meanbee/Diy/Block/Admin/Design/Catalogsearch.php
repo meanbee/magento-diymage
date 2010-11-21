967180e290f655e138e64f6ee4aebb0169bf1aa3
Tom Robertshaw <tom.robertshaw@meanbee.com>
2010-11-21 11:33:45 +0000

    Added a catalog search config page.

---
+++ app/code/local/Meanbee/Diy/Block/Admin/Design/Catalogsearch.php
@@ -0,0 +1,6 @@
+<?php
+class Meanbee_Diy_Block_Admin_Design_Catalogsearch extends Meanbee_Diy_Block_Admin_Design_Abstract {
+    protected function getDataGroup() {
+        return "catalogsearch_result_index";
+    }
+}
