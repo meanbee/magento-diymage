a78f97ee4c4ec8d69a462df8aea6c4a4894618df
Nick Jones <nick@nicksays.co.uk>
2010-11-21 11:29:23 +0000

    [Fixed: 17] Add the ability to switch logging on and off

--- app/code/local/Meanbee/Diy/Model/Log.php
+++ app/code/local/Meanbee/Diy/Model/Log.php
@@ -2,12 +2,18 @@
 class Meanbee_Diy_Model_Log {
     protected $_fileName;
     protected $_indent = 0;
+    protected $_enabled = false;
     
     public function __construct() {
         $this->_fileName = "diymage.log";
+        $this->_enabled = Mage::getSingleton('diy/settings')->isLoggingEnabled();
     }
     
     protected function _writeLog($message, $level) {
+        if (!$this->_enabled) {
+            return;
+        }
+        
         if ($this->_indent > 0) {
             $indent_size = strlen($message) + ($this->_indent * 4);
             $message = str_pad($message, $indent_size, " ", STR_PAD_LEFT);
--- app/code/local/Meanbee/Diy/Model/Settings.php
+++ app/code/local/Meanbee/Diy/Model/Settings.php
@@ -3,4 +3,8 @@ class Meanbee_Diy_Model_Settings {
     public function isEnabled() {
         return Mage::getStoreConfig('diy/general/enabled');
     }
+    
+    public function isLoggingEnabled() {
+        return $this->isEnabled() && Mage::getStoreConfig('diy/general/log_enabled');
+    }
 }
