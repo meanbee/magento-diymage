857dbf5c1704d1caaf42e175809a2c7778ed230e
Nick Jones <nick@nicksays.co.uk>
2010-12-05 23:34:46 +0000

    Move the help text and link out into its own block.

---
+++ app/code/local/Meanbee/Diy/Block/Admin/Helplink.php
@@ -0,0 +1,10 @@
+<?php
+class Meanbee_Diy_Block_Admin_Helplink extends Meanbee_Diy_Block_Admin_Abstract {
+    public function getHelpLink() {
+        return "http://docs.diymage.com/Home.html";
+    }
+    
+    public function getHelpText() {
+        return "Get help with DIY Mage";
+    }
+}
