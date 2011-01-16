81fb0063a40cd90dad7a0ef30ab14cea303d4d62
Nick Jones <nick@nicksays.co.uk>
2011-01-16 21:16:28 +0000

    Stop duplicating the stores in the dropdown.

--- app/code/local/Meanbee/Diy/Block/Admin/Switcher.php
+++ app/code/local/Meanbee/Diy/Block/Admin/Switcher.php
@@ -5,9 +5,11 @@ class Meanbee_Diy_Block_Admin_Switcher extends Meanbee_Diy_Block_Admin_Abstract 
         $options = array();
         
         foreach ($storeModel->getWebsiteCollection() as $website) {
-           foreach ($storeModel->getGroupCollection() as $group) {
-               foreach ($storeModel->getStoreCollection() as $store) {
-                   $options[$store->getId()] = $store->getName() . " (" . $website->getName() . ")";
+           foreach ($storeModel->getGroupCollection() as $store) {
+               if ($store->getWebsiteId() != $website->getId()) { continue; }
+               foreach ($storeModel->getStoreCollection() as $view) {
+                   if ($view->getGroupId() != $store->getId()) { continue; }
+                   $options[$view->getId()] = $view->getName() . " (" . $website->getName() . "/" . $store->getName() . ")";
                }
            }
         }
