8707075b1c5fb051f8f7429e2109726362575b75
Nick Jones <nick@nicksays.co.uk>
2011-10-19 13:34:07 +0100

    Added functions to the developer bar

--- code/controllers/DevtoolsController.php
+++ code/controllers/DevtoolsController.php
@@ -28,4 +28,34 @@ class Meanbee_Diy_DevtoolsController extends Mage_Core_Controller_Front_Action {
         
         $this->_redirectReferer();
     }
+    
+    public function templateToggleAction() {
+        $config_paths = array(
+            'dev/debug/template_hints',
+            'dev/debug/template_hints_blocks'
+        );
+        
+        foreach ($config_paths as $path) {
+            $this->_toggleConfig($path);
+        }
+        
+        $this->_redirectReferer();
+    }
+    
+    public function loggingToggleAction() {
+        $this->_toggleConfig('dev/log/active');
+        $this->_redirectReferer();
+    }
+    
+    public function symlinkToggleAction() {
+        $this->_toggleConfig('dev/template/allow_symlink');
+        $this->_redirectReferer();
+    }
+    
+    protected function _toggleConfig($path) {
+        $old_value = Mage::getStoreConfigFlag($path);
+        $new_value = !$old_value;
+        
+        Mage::getSingleton('core/config')->saveConfig($path, $new_value);
+    }
 }
