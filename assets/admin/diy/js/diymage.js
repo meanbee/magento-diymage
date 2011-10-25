5c2b4ab5be6ceff3ef3bdf388494ed8e2de9112e
Tom Robertshaw <me@tomrobertshaw.net>
2011-10-25 21:18:36 +0100

    Make reset to default colour links functional.

--- assets/admin/diy/js/diymage.js
+++ assets/admin/diy/js/diymage.js
@@ -1,4 +1,4 @@
 // Used in the control section to reset the value to the origin value
 function resetValue(id, value) {
-    $j("#" + id).val(value);
+    $j.fn.mColorPicker.setInputColor(id, "#" + value);
 }
