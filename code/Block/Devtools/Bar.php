f71a8a1fe3a1ee98e488b68611d0a24a340e804e
Nick Jones <nick@nicksays.co.uk>
2011-11-05 16:44:45 +0000

    Ensure we load the adminhtml dev tool templates

--- code/Block/Devtools/Bar.php
+++ code/Block/Devtools/Bar.php
@@ -1,9 +1,8 @@
 <?php
 // {{license}}
-class Meanbee_Diy_Block_Devtools_Bar extends Meanbee_Diy_Block_Abstract {
-    public function __construct() {
-        parent::__construct();
-
+class Meanbee_Diy_Block_Devtools_Bar extends Meanbee_Diy_Block_Devtools_Abstract {
+    public function _construct() {
+        parent::_construct();
         $this->setTemplate('diy/devtools/bar.phtml');
     }
 }
