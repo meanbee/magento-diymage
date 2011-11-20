50904881799ad9e389ba78afc7de928ca2eb86f9
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-20 21:41:13 +0000

    Add billing agreements and recurring profiles (only top levels).

---
+++ code/Block/Admin/Design/Recurringprofiles.php
@@ -0,0 +1,9 @@
+<?php
+// {{license}}
+class Meanbee_Diy_Block_Admin_Design_Recurringprofiles extends Meanbee_Diy_Block_Admin_Design_Abstract {
+    protected $_title = "Customer Recurring Profiles Design Settings";
+    
+    protected function getDataGroup() {
+        return "sales_recurring_profile_index";
+    }
+}
