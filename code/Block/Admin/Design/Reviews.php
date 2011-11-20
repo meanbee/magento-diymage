07ed62e0a51996cb313dfaa3e773bac09f9d46f0
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-20 21:48:26 +0000

    Add customer reviews.

---
+++ code/Block/Admin/Design/Reviews.php
@@ -0,0 +1,9 @@
+<?php
+// {{license}}
+class Meanbee_Diy_Block_Admin_Design_Reviews extends Meanbee_Diy_Block_Admin_Design_Abstract {
+    protected $_title = "Customer Reviews Design Settings";
+    
+    protected function getDataGroup() {
+        return "review_customer_index";
+    }
+}
