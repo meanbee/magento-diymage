178a8ffba2f2f7247256448b8d483031e3c10a51
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-20 20:55:12 +0000

    Missed account info block.

---
+++ code/Block/Admin/Design/Accountinfo.php
@@ -0,0 +1,9 @@
+<?php
+// {{license}}
+class Meanbee_Diy_Block_Admin_Design_Accountinfo extends Meanbee_Diy_Block_Admin_Design_Abstract {
+    protected $_title = "Customer Account Information Design Settings";
+    
+    protected function getDataGroup() {
+        return "customer_account_edit";
+    }
+}
