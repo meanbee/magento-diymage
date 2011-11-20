fb452c5ff3562a4ea8f6de3553cdce0d7a645b10
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-20 23:21:38 +0000

    Add layered category page.

--- code/controllers/DesignController.php
+++ code/controllers/DesignController.php
@@ -38,6 +38,10 @@ class Meanbee_Diy_DesignController extends Mage_Adminhtml_Controller_Action {
         $this->__render();
     }
     
+    public function layeredlistingAction() {
+        $this->__render();
+    }
+    
     public function productAction() {
         $this->__render();
     }
