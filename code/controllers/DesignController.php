9e919c21812809d2c62d26d174fe4d649c4c86a6
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-20 21:26:29 +0000

    Add Account create page.

--- code/controllers/DesignController.php
+++ code/controllers/DesignController.php
@@ -66,6 +66,10 @@ class Meanbee_Diy_DesignController extends Mage_Adminhtml_Controller_Action {
         $this->__render();
     }
     
+    public function accountcreateAction() {
+        $this->__render();
+    }
+    
     public function accountdashboardAction() {
         $this->__render();
     }
