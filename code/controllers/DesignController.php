36cea6895c8fd0081913bedbb78085ebd9ae954d
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-20 22:42:17 +0000

    Add Onepage Order Confirmation Page.

--- code/controllers/DesignController.php
+++ code/controllers/DesignController.php
@@ -50,6 +50,10 @@ class Meanbee_Diy_DesignController extends Mage_Adminhtml_Controller_Action {
         $this->__render();
     }
     
+    public function checkoutonepagesuccessAction() {
+        $this->__render();
+    }
+    
     public function cartAction() {
         $this->__render();
     }
