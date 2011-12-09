29dfd95ffbbe32bea16759493c9cc9c0c3eddab5
Nick Jones <nick@nicksays.co.uk>
2011-12-09 18:17:24 +0000

    Fix help link

--- code/Block/Admin/Helplink.php
+++ code/Block/Admin/Helplink.php
@@ -2,7 +2,7 @@
 // {{license}}
 class Meanbee_Diy_Block_Admin_Helplink extends Meanbee_Diy_Block_Admin_Abstract {
     public function getHelpLink() {
-        return "http://docs.diymage.com/Home.html";
+        return "http://docs.diymage.com";
     }
     
     public function getHelpText() {
