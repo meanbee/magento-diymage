02bddc2dd47efc3a3519eb71305306e2854c1324
Nick Jones <nick@nicksays.co.uk>
2011-12-09 16:51:41 +0000

    Remove block hints

--- code/Entity/Setup.php
+++ code/Entity/Setup.php
@@ -1,7 +1,7 @@
 <?php
 // {{license}}
 class Meanbee_Diy_Entity_Setup extends Mage_Eav_Model_Entity_Setup {
-    protected function populateData() {
+    public function populateData() {
         return Mage::getSingleton('diy/xml')->repopulateData();
     }
     
--- code/Model/Observer/Devtools.php
+++ code/Model/Observer/Devtools.php
@@ -16,11 +16,13 @@ class Meanbee_Diy_Model_Observer_Devtools implements Meanbee_Diy_Model_Observer_
         if (!Mage::getSingleton('diy/config')->isDeveloperToolbarEnabled()) {
             return;
         }
-        
-        $block_bar = Mage::app()->getLayout()->createBlock('diy/devtools_bar');
-        Mage::app()->getLayout()->getBlock('content')->append($block_bar);
-        
-        $block_hints = Mage::app()->getLayout()->createBlock('diy/devtools_hints');
-        Mage::app()->getLayout()->getBlock('content')->append($block_hints);
+
+        if (Mage::app()->getLayout()->getArea() != 'adminhtml') {
+            $block_bar = Mage::app()->getLayout()->createBlock('diy/devtools_bar');
+            Mage::app()->getLayout()->getBlock('content')->append($block_bar);
+        }
+//
+//        $block_hints = Mage::app()->getLayout()->createBlock('diy/devtools_hints');
+//        Mage::app()->getLayout()->getBlock('content')->append($block_hints);
     }
 }
