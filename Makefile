0f89d8ef9081ecd9b4f7db01c3e7422a7729b47c
Nick Jones <nick@nicksays.co.uk>
2012-01-03 14:31:49 +0000

    Set up a passing test, and a failing test

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	cd tests && phpunit --configuration phpunit.xml
---
+++ tests/functional/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php
+class ExampleTest extends Mage_Selenium_TestCase {
+    /**
+     * @test
+     */
+    public function exampleTests() {
+        $this->assertTrue(false);
+    }
+}
