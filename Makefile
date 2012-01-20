e0b40fa237ad4c0385eee58fe5790d83aa2c65f8
Nick Jones <nick@nicksays.co.uk>
2012-01-20 16:24:27 +0000

    Revert "Set up a passing test, and a failing test"
    This reverts commit 0f89d8ef9081ecd9b4f7db01c3e7422a7729b47c.

--- Makefile
+++
@@ -1,2 +0,0 @@
-test:
-	cd tests && phpunit --configuration phpunit.xml
--- tests/functional/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ExampleTest extends Mage_Selenium_TestCase {
-    /**
-     * @test
-     */
-    public function exampleTests() {
-        $this->assertTrue(false);
-    }
-}
