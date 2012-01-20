cd88f7d14812aca1c6d72bd49de0a2dee208f575
Nick Jones <nick@nicksays.co.uk>
2012-01-20 16:04:33 +0000

    Move the template hint CSS to classes instead of inline.

--- code/Model/Observer/Devtools/Hints.php
+++ code/Model/Observer/Devtools/Hints.php
@@ -52,19 +52,17 @@ class Meanbee_Diy_Model_Observer_Devtools_Hints implements Meanbee_Diy_Model_Obs
                 );
 
                 echo <<<HTML
-<div style="position:relative; border:1px dotted red; margin:6px 2px; padding:18px 2px 2px 2px; zoom:1;">
-<div style="position:absolute; left:0; top:0; padding:2px 5px; background:red; color:white; font:normal 11px Arial;
-text-align:left !important; z-index:998;" onmouseover="this.style.zIndex='999'"
-onmouseout="this.style.zIndex='998'" title="{$template}">{$info_string}</div>
-HTML;
-
-                echo <<<HTML
-<div style="position:absolute; right:0; top:0; padding:2px 5px; background:red; color:blue; font:normal 11px Arial;
-text-align:left !important; z-index:998;" onmouseover="this.style.zIndex='999'" onmouseout="this.style.zIndex='998'"
-title="{$class_php_name}">{$class_php_name} ({$class_xml_type})</div>
+<div class="diymage-hint">
+    <div class="diymage-hint-template" onmouseover="this.style.zIndex='999'"onmouseout="this.style.zIndex='998'" title="{$template}">
+        {$info_string}
+    </div>
+    <div class="diymage-hint-class" onmouseover="this.style.zIndex='999'" onmouseout="this.style.zIndex='998'"title="{$class_php_name}">
+        {$class_php_name} ({$class_xml_type})
+    </div>
 HTML;
                 break;
             case self::EVENT_AFTER:
+                //
                 echo "</div>";
                 break;
         }
