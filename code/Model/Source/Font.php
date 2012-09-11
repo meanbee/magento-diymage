0337aea99cbc82d4bdaab35d86cc83454fe0b5f7
Tom Robertshaw <me@tomrobertshaw.net>
2012-09-11 11:46:14 +0100

    Allow font to be reset to default by including an empty option.

--- code/Model/Source/Font.php
+++ code/Model/Source/Font.php
@@ -3,6 +3,7 @@
 class Meanbee_Diy_Model_Source_Font implements Meanbee_Diy_Model_Source_Interface {
     public function asArray() {
         $result = array(
+            "",
             "arial",
             "courier",
             "georgia",
